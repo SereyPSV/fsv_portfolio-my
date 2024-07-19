b44c0c6d2c773f9a4445161e21db98436ad52fd4
sereypsv <valid_@tut.by>
2024-07-19 09:54:47 +0300

    feat: edit heder-about font-color

--- src/shared/ui/header/components/header-about/header-about.tsx
+++ src/shared/ui/header/components/header-about/header-about.tsx
@@ -1,16 +1,16 @@
 export const HeaderAbout = () => {
   return (
     <div id="about" className="flex p-32">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center text-[var(--app-background)]">
         <h1 className="text-4xl font-bold pb-8 text-center">
-          <span className="">Experienced </span>
+          <span className="text-[var(--app-background)]">Experienced </span>
           <span className="text-[var(--primary-btn)]">mobile and web </span>
           <br />
           <span className="">
             applications and website <br /> builders measuring.
           </span>
         </h1>
-        <h4 className="text-2xl font-bold pb-8">
+        <h4 className="text-2xl pb-8 font-thin text-[var(--primary-input)]">
           KODEX TECHNOLOGY (PVT) LTD is a team of experienced mobile and web
           applications and website builders measuring dozens of completed
           projects. We build and develop mobile applications for several top
