ce31740d79beb43bf5d5147350b99046f63f7060
sereypsv <valid_@tut.by>
2024-07-19 22:18:32 +0300

    refactor: refactor header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { About, Footer, HeaderAbout, HeaderNav, Solutions } from "@/shared/ui";
 export default function Home() {
   return (
     <main className="max-w-[1240px] mx-auto">
-      {/* <HeaderNav /> */}
+      <HeaderNav />
       <HeaderAbout />
       <About />
       <Solutions />
